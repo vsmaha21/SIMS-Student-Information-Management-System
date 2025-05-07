9d81de2f97024f17e417849532ba789fcb1aec34
vsmaha21 <vsmahamuni21@gmail.com>
2025-05-07 23:51:01 +0530

    Spring Security Config

---
+++ src/main/java/com/college/mis/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.college.mis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("/college/**").hasRole("ADMIN").anyRequest().permitAll())
+				.httpBasic();
+		return http.build();
+
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		return new InMemoryUserDetailsManager(
+				User.builder().username("admin").password(encoder.encode("admin123")).roles("ADMIN").build());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
